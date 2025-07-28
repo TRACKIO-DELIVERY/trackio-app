a39a7418aa6773c4843821b211b3bc9f91c86acf
Lucas Gabriel de Queiroz Souza <lucasgabriel20032013@gmail.com>
2025-07-28 15:07:04 -0300

    Feat: Criação dos cards de order para o app

---
+++ src/components/Atoms/iconImage/index.tsx
@@ -0,0 +1,12 @@
+import { View, StyleSheet } from 'react-native';
+import { styles } from "./styles";
+
+export const OrderIcon: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.icon} />
+    </View>
+  );
+};
+
+
---
+++ src/components/Atoms/iconImage/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native-unistyles";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    marginRight: 12,
+  },
+  icon: {
+    width: 84,
+    height: 84,
+    borderRadius: 8,
+    backgroundColor: '#e0e0e0',
+  },
+});
